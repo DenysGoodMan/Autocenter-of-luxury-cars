90b6357fa7c6f04671e8d7287129ae35b00d76fe
DenysGoodMan <hromskyidenys@gmail.com>
2024-07-09 22:05:35 +0300

    first commit

---
+++ JavaScript/info.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('.header_info_offer-slick-slider').slick({
+        slidesToShow: 1,
+        //   speed: 300
+        slidesToScroll: 1, 
+    });
+ 
+});
+
---
+++ JavaScript/slider.js
@@ -0,0 +1,39 @@
+
+// const menu = document.getElementById("menu");
+$(function(){
+
+    $('.model_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1, 
+        //dots: true,
+        //speed: 300
+    });
+
+});
+
+
+
+// document.querySelector('.burger').addEventListener('click',function(){
+//     this.classList.toggle('active');
+//     document.querySelector('.nav').classList.toggle('open');
+// })
+
+// menu.addEventListener("click", (e) => {
+//     menu.classList.remove("open");
+//   });
+
+
+// $(document).on('click', '#burger',function(){
+//     $('.nav_menu').toggleeClass('active');
+// });
+
+
+
+
+// const burger = document.getElementById('burger');
+// const menu = document.getElementById('menu');
+
+// burger.addEventListener('click', () => {
+//   burger.classList.toggle('active');
+//   menu.classList.toggle('active');
+// });
